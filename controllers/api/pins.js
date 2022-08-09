615a03ec28410c9edfeee2f62cdd84cd73e5817e
Asti <astistateofmind@gmail.com>
2022-08-09 11:00:04 -0700

    add show all func for the pins

--- controllers/api/pins.js
+++ controllers/api/pins.js
@@ -2,7 +2,8 @@ const Pin = require('../../models/pin')
 
 
 module.exports = {
-    create
+    create,
+    show
 }
 
 //create new pin
@@ -12,6 +13,15 @@ async function create(req, res) {
         //await saving if sucessfull => return json
         const savedPin = await newPin.save();
         res.status(200).json(savedPin);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+async function show(req, res) {
+    try {
+        const pins = await Pin.find();
+        res.status(200).json(pins);
     } catch (err) {
         res.status(500).json(err)
     }
--- routes/api/pins.js
+++ routes/api/pins.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const pinsCtrl = require('../../controllers/api/pins');
 
 router.post('/', pinsCtrl.create);
+router.get('/', pinsCtrl.show);
 
 module.exports = router
