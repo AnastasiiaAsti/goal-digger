6fceae40773053f2776f7b4bd2f453a19207690f
Asti <astistateofmind@gmail.com>
2022-08-09 11:45:40 -0700

    adding a miap API

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useState } from "react";
+import * as React from 'react';
+import Map, {Marker} from 'react-map-gl';
 import { Routes, Route } from "react-router-dom";
 import { getUser } from "../../utilities/users-service";
 import AuthPage from "../AuthPage/AuthPage";
@@ -9,6 +11,11 @@ import GoalListPage from '../GoalListPage/GoalListPage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+  const [viewState, setViewState] = React.useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14
+  });
 
   return (
     <main className="App">
@@ -19,6 +26,15 @@ export default function App() {
             <Route path="/lists" element={<GoalListPage user={user} setUser={setUser} />} />
             <Route path="/lists/new" element={<AddToListPage user={user} setUser={setUser}/>} />
           </Routes>
+          <Map
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAP}
+    >
+      <Marker longitude={-122.4} latitude={37.8} color="red" />
+    </Map>
         </>
       ) :
         <AuthPage setUser={setUser} />
