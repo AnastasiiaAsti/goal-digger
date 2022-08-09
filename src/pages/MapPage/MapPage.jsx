e08b768e7a7f03702dfbfe65690df2e719ba6412
Asti <astistateofmind@gmail.com>
2022-08-09 13:06:06 -0700

    added marker

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -2,6 +2,7 @@ import Map, {Marker} from 'react-map-gl';
 import * as React from 'react';
 
 
+
 export default function MapPage() {
     const [viewState, setViewState] = React.useState({
         latitude: 33.9799,
@@ -16,7 +17,7 @@ export default function MapPage() {
             mapStyle="mapbox://styles/mapbox/streets-v9"
             mapboxAccessToken={process.env.REACT_APP_MAP}
         >
-            <Marker longitude={-118.4370} latitude={33.9799} >
+            <Marker longitude={-118.4370} latitude={33.9799} color="var(--peach)">
     </Marker>  
     </Map>
     )
